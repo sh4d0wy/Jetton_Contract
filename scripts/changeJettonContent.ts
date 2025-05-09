8efffca1abe14171816465b74a1221266ca13215
sh4d0wy <sakshambhugra8@gmail.com>
2025-05-09 15:53:36 +0530

    Chore: Added dynamically added content uri

--- scripts/changeJettonContent.ts
+++ scripts/changeJettonContent.ts
@@ -1,12 +1,12 @@
 import { beginCell, toNano } from '@ton/core';
 import { JettonMinter } from '../wrappers/JettonMinter';
 import { NetworkProvider } from '@ton/blueprint';
-import { JETTON_MINTER_ADDRESS } from '../config';
+import { JETTON_CONTENT_URI, JETTON_MINTER_ADDRESS } from '../config';
 
 export async function run(provider: NetworkProvider) {
     const content = {
         type: 1,
-        uri: "https://olive-fashionable-mule-815.mypinata.cloud/ipfs/bafkreic7rpannnli7lfoumh47zhsnrbtoqmdf66sz6alhmj74at4ml7lmy"
+        uri: JETTON_CONTENT_URI
     };
     
     const contentCell = beginCell()
--- scripts/deployJettonMinter.ts
+++ scripts/deployJettonMinter.ts
@@ -1,6 +1,7 @@
 import { beginCell, toNano } from '@ton/core';
 import { JettonMinter } from '../wrappers/JettonMinter';
 import { NetworkProvider } from '@ton/blueprint';
+import { JETTON_CONTENT_URI } from '../config';
 
 export async function run(provider: NetworkProvider) {
     const senderAddress = await provider.sender().address;
@@ -10,7 +11,7 @@ export async function run(provider: NetworkProvider) {
 
     const content = {
         type:1,
-        uri:"https://olive-fashionable-mule-815.mypinata.cloud/ipfs/bafkreic7rpannnli7lfoumh47zhsnrbtoqmdf66sz6alhmj74at4ml7lmy"
+        uri:JETTON_CONTENT_URI
     }
     const cell = beginCell()
             .storeUint(content.type, 8)

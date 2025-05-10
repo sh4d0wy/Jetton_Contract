d5ce001d072907d24dde3d2c2142db701c6db1bb
sh4d0wy <sakshambhugra8@gmail.com>
2025-05-10 16:28:10 +0530

    chore: Updated minting logic

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 import { Address } from '@ton/core';
 
-export const JETTON_MINTER_ADDRESS = Address.parse('EQBDcfsZW5DGz7mlJO9hOetx-5X6-uWiCVj20NrpnWpc01x9');  
+export const JETTON_MINTER_ADDRESS = Address.parse('EQBs1vOqcTTOrX0pm3JGo76bzdYFsw1wD0xE3aKXhz_EdcnP');  
 
 //IPFS URI for the token metadata
 export const JETTON_CONTENT_URI = "https://olive-fashionable-mule-815.mypinata.cloud/ipfs/bafkreic7rpannnli7lfoumh47zhsnrbtoqmdf66sz6alhmj74at4ml7lmy"
--- scripts/deployJettonMinter.ts
+++ scripts/deployJettonMinter.ts
@@ -1,7 +1,7 @@
 import { beginCell, toNano } from '@ton/core';
 import { JettonMinter } from '../wrappers/JettonMinter';
 import { NetworkProvider } from '@ton/blueprint';
-import { JETTON_CONTENT_URI } from '../config';
+import { JETTON_CONTENT_URI, JETTON_MINTER_ADDRESS } from '../config';
 
 export async function run(provider: NetworkProvider) {
     const senderAddress = await provider.sender().address;
@@ -18,18 +18,33 @@ export async function run(provider: NetworkProvider) {
             .storeStringTail(content.uri) 
             .endCell()
     
-    
-    const jettonMinter = provider.open(await JettonMinter.fromInit(100000000n, senderAddress, cell,true));
+    const amount = toNano('1000000000');
+    const forwardAmount = toNano('0.1');
+    const totalAmount = toNano('0.1');
+    const jettonMinter = provider.open(await JettonMinter.fromInit(0n, senderAddress, cell,true));
 
     await jettonMinter.send(
         provider.sender(),
         {
-            value: toNano('0.05'),
+            value: totalAmount+toNano('0.05'),
         },
-        null,
+        {
+            $$type: 'Mint',
+            queryId: 0n,
+            receiver: provider.sender().address!,
+            tonAmount: totalAmount,
+            mintMessage: {
+                $$type: 'JettonTransferInternal',
+                queryId: 0n,
+                amount: amount,
+                sender: jettonMinter.address!,
+                responseDestination: jettonMinter.address!,
+                forwardTonAmount: forwardAmount,
+                forwardPayload: beginCell().storeUint(0,8).endCell().asSlice()
+            }
+        }
     );
 
     await provider.waitForDeploy(jettonMinter.address);
 
-    // run methods on `jettonMinter`
 }
